6969bae8e1b16d695ac162dec3d4c27a2f565c4e
kbrakke <kbrakke@gmail.com>
2016-05-19 19:09:14 -0400

    Change to React for the front end

---
+++ app/helloworld.js
@@ -0,0 +1,14 @@
+/**
+ * Created by kbrak on 5/19/2016.
+ */
+require('react');
+require('react-dom');
+
+class HelloComponent extends React.component {
+    
+}
+
+ReactDOM.render(
+    <h1>Hello World!</h1>,
+    document.getElementById('example')
+);
